e6f9ec833053070bff2a2be021332c680fffab44
Alberto Monteiro <alberto.monteiro@live.com>
2016-01-02 23:01:58 -0300

    Still working on UI

---
+++ src/Duget/ViewModel/DugetPackage.cs
@@ -0,0 +1,11 @@
+namespace Duget.ViewModel
+{
+    public class DugetPackage
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Version { get; set; }
+    }
+}
--- src/Duget/ViewModel/MainViewModel.cs
+++ src/Duget/ViewModel/MainViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 using PropertyChanged;
 
@@ -8,15 +9,34 @@ namespace Duget.ViewModel
     {
         public MainViewModel()
         {
-            if (IsInDesignMode)
+            if (IsInDesignMode || IsInDesignModeStatic)
             {
                 ProjectName = "Cargas";
-                ShowNoItemsFound = true;
+                ShowNoItemsFound = false;
                 ShowLoading = false;
+
+                Packages = new ObservableCollection<DugetPackage>
+                {
+                    new DugetPackage {
+                        Author = "Microsoft",
+                        Description = "Entity Framework is Microsoft's recommended data access technology for new applications.",
+                        Name = "EntityFramework",
+                        Version = "6.1.3",
+                        Image = "http://go.microsoft.com/fwlink/?LinkID=386613"
+                    },
+                    new DugetPackage {
+                        Author = "James Newton-King",
+                        Description = "Json.NET is a popular high-performance JSON framework for .NET",
+                        Name = "Json.NET",
+                        Version = "8.0.1",
+                        Image = "http://www.newtonsoft.com/content/images/nugeticon.png"
+                    }
+                };
             }
             else
             {
                 ProjectName = "Cargas";
+                ShowLoading = true;
                 // Code runs "for real"
             }
         }
@@ -26,5 +46,7 @@ namespace Duget.ViewModel
         public bool ShowNoItemsFound { get; set; }
 
         public bool ShowLoading { get; set; }
+
+        public ObservableCollection<DugetPackage> Packages { get; set; }
     }
 }
