c88f0165ecac6b649ae5d2f1f345ed90226cc4f4
Alberto Monteiro <alberto.monteiro@live.com>
2016-01-04 16:31:53 -0300

    First draft of Nuget service access

--- src/Duget/ViewModel/DugetPackage.cs
+++ src/Duget/ViewModel/DugetPackage.cs
@@ -1,11 +1,44 @@
+using NuGet;
+using static System.String;
+
 namespace Duget.ViewModel
 {
     public class DugetPackage
     {
+        public DugetPackage() { }
+
+        public DugetPackage(string name, string author, string description, string image, string version)
+        {
+            Name = name;
+            Author = author;
+            FullDescription = description;
+            Image = image;
+            Version = version;
+        }
+
         public string Name { get; set; }
         public string Author { get; set; }
-        public string Description { get; set; }
+        public string Description => FullDescription.Substring(0, 200);
+        public string FullDescription { get; set; }
         public string Image { get; set; }
         public string Version { get; set; }
+
+        public static DugetPackage FromIPackage(IPackage package)
+            => new DugetPackage(package.Title, Join(", ", package.Authors), package.Description, package.IconUrl?.ToString(), package.Version?.ToString());
+
+        protected bool Equals(DugetPackage other) => string.Equals(Name, other.Name);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DugetPackage)obj);
+        }
+
+        public override int GetHashCode() => Name?.GetHashCode() ?? 0;
+
+        public static bool operator ==(DugetPackage left, DugetPackage right) { return Equals(left, right); }
+        public static bool operator !=(DugetPackage left, DugetPackage right) { return !Equals(left, right); }
     }
 }
--- src/Duget/ViewModel/MainViewModel.cs
+++ src/Duget/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 using GalaSoft.MvvmLight;
+using NuGet;
 using PropertyChanged;
 
 namespace Duget.ViewModel
@@ -22,24 +24,34 @@ namespace Duget.ViewModel
                 {
                     new DugetPackage {
                         Author = "Microsoft",
-                        Description = "Entity Framework is Microsoft's recommended data access technology for new applications.",
+                        FullDescription = "Entity Framework is Microsoft's recommended data access technology for new applications.",
                         Name = "EntityFramework",
                         Version = "6.1.3",
                         Image = "http://go.microsoft.com/fwlink/?LinkID=386613"
                     },
                     new DugetPackage {
                         Author = "James Newton-King",
-                        Description = "Json.NET is a popular high-performance JSON framework for .NET",
+                        FullDescription = "Json.NET is a popular high-performance JSON framework for .NET",
                         Name = "Json.NET",
                         Version = "8.0.1",
                         Image = "http://www.newtonsoft.com/content/images/nugeticon.png"
+                    },
+                    new DugetPackage {
+                        Author = "jQuery Foundation,  Inc.",
+                        FullDescription = @"jQuery is a new kind of JavaScript Library.
+        jQuery is a fast and concise JavaScript Library that simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development. jQuery is designed to change the way that you write JavaScript.
+        NOTE: This package is maintained on behalf of the library owners by the NuGet Community Packages project at http://nugetpackages.codeplex.com/",
+                        Name = "jQuery",
+                        Version = "2.1.4"
                     }
                 };
             }
             else
             {
                 ProjectName = "Cargas";
-                ShowLoading = true;
+                ShowLoading = false;
+                ListType = ListType.Browse;
+                LoadPackages();
             }
         }
 
@@ -58,5 +70,21 @@ namespace Duget.ViewModel
         public ListType ListType { get; set; }
 
         public ObservableCollection<DugetPackage> Packages { get; set; }
+
+
+        public void LoadPackages()
+        {
+            Packages = new ObservableCollection<DugetPackage>();
+
+            //Connect to the official package repository
+            var repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+
+            //Get the list of all NuGet packages with ID 'EntityFramework'       
+            var packages = repo.Search("", false).Take(10).ToList().ToList().Select(DugetPackage.FromIPackage);
+
+            //Iterate through the list and print the full name of the pre-release packages to console
+            foreach (var p in packages)
+                Packages.Add(p);
+        }
     }
 }
